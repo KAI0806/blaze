ebf30188f016e86f1cea228578c1e29579a72490
甲斐聖健 <>
2022-04-14 12:39:32 +0900

    細部調整

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,10 +2,15 @@ function hamburger(){
     document.getElementById('ladder').classList.toggle('ladder_white');
     document.getElementById('nav').classList.toggle('in');
     document.getElementById('logo').classList.toggle('white');
-  };
-  document.getElementById('hamburger').addEventListener('click',function(){
-    hamburger();
-  });
-  document.getElementById('nav').addEventListener('click',function(){
-    hamburger();
-  });
+};
+
+function slideup(){
+  document.getElementById('ladder').classList.toggle('ladder_white');
+};
+
+document.getElementById('hamburger').addEventListener('click',function(){
+  hamburger();
+});
+document.getElementById('nav').addEventListener('click',function(){
+  hamburger();
+});

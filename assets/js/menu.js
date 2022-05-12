2d40d60a8910a594235e1106f73391f5b5a3a43e
甲斐聖健 <>
2022-05-12 15:09:36 +0900

    画像最適化

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,8 +4,17 @@ function hamburger(){
     document.getElementById('logo').classList.toggle('white');
 };
 
-function slideup(){
-  document.getElementById('ladder').classList.toggle('ladder_white');
+function slideup01(){
+  document.getElementById('slide_img1').classList.toggle('slide_active');
+};
+function slideup02(){
+  document.getElementById('slide_img2').classList.toggle('slide_active');
+};
+function slideup03(){
+  document.getElementById('slide_img3').classList.toggle('slide_active');
+};
+function slideup04(){
+  document.getElementById('slide_img4').classList.toggle('slide_active');
 };
 
 document.getElementById('hamburger').addEventListener('click',function(){
@@ -13,4 +22,20 @@ document.getElementById('hamburger').addEventListener('click',function(){
 });
 document.getElementById('nav').addEventListener('click',function(){
   hamburger();
+});
+
+document.getElementsByClassName('slideup1').addEventListener('click',function(){
+  slideup01();
+});
+
+document.getElementsByClassName('slideup2').addEventListener('click',function(){
+  slideup02();
+});
+
+document.getElementsByClassName('slideup3').addEventListener('click',function(){
+  slideup03();
+});
+
+document.getElementsByClassName('slideup4').addEventListener('click',function(){
+  slideup04();
 });
